d4be85dcc3950301bb7d6bd21fe3f9e5757f8ff3
Obed N Munoz <obed.n.munoz@intel.com>
2017-08-18 15:26:10 -0500

    Fix staticcheck linter warnings
    Fixes: #13
    
    Signed-off-by: Obed N Munoz <obed.n.munoz@intel.com>

--- template.go
+++ template.go
@@ -52,31 +52,25 @@ func NewVMTemplate(c *VMTemplate) VMTemplate {
 	/* Docker network definitions */
 	if len(c.Networks) > 0 {
 		for _, net := range c.Networks {
-			//err = docker.VerifyNetwork(ctx, cli, net.Name)
-
-			if 1 != 1 {
+			_, err := cli.NetworkCreate(ctx, net.Name, types.NetworkCreate{
+				CheckDuplicate: true,
+				IPAM: &network.IPAM{
+					Config: []network.IPAMConfig{
+						{Subnet: net.Subnet},
+					},
+				},
+				Options: map[string]string{
+					"com.docker.network.bridge.enable_icc":           "true",
+					"com.docker.network.bridge.enable_ip_masquerade": "true",
+					"com.docker.network.bridge.host_binding_ipv4":    "0.0.0.0",
+					"com.docker.network.bridge.name":                 net.Name,
+					"com.docker.network.driver.mtu":                  "1500",
+				},
+			})
+			if err != nil {
 				glog.Info(err)
-			} else {
-				_, err := cli.NetworkCreate(ctx, net.Name, types.NetworkCreate{
-					CheckDuplicate: true,
-					IPAM: &network.IPAM{
-						Config: []network.IPAMConfig{
-							{Subnet: net.Subnet},
-						},
-					},
-					Options: map[string]string{
-						"com.docker.network.bridge.enable_icc":           "true",
-						"com.docker.network.bridge.enable_ip_masquerade": "true",
-						"com.docker.network.bridge.host_binding_ipv4":    "0.0.0.0",
-						"com.docker.network.bridge.name":                 net.Name,
-						"com.docker.network.driver.mtu":                  "1500",
-					},
-				})
-				if err != nil {
-					glog.Info(err)
-				}
-				glog.Flush()
 			}
+			glog.Flush()
 		}
 	}
 
